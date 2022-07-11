cbd1bcd8a6ca8815802d49d2617108e68c32e04e
Victor Anolu <109082320+victoranolu@users.noreply.github.com>
2022-07-11 19:15:29 +0000

    Added a for loop to iterate the array to sum the numbers up and return the even numbers in the arry.

--- src/assignment.js
+++ src/assignment.js
@@ -14,23 +14,33 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    let sum = 0;
+  for(let index = 0; index < arrayOfNumbers.length; index++) {
+    sum += arrayOfNumbers[index]
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+  return sum;
+}
+ assignment.sumOfNumbers = sumOfNumbers;
+
 
 /**
  * Challenge - 2
  * 
  * Count all even numbers within an array of numbers.
  * If you did Challenge - 2, remove the comment in the line just after this function
- * 
  * @param {Array} arrayOfNumbers the array containing even or non-even numbers
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let evenNumber = 0;
+  for (let i = 0; i < arrayOfNumbers.length; i++){
+    if (arrayOfNumbers[i] % 2 == 0){
+       evenNumber++
+    }
+  }
+  return evenNumber;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
